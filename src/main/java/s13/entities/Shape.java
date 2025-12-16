65ceb4fea4361adbc3f132db2378e980eb14baf2
allandpn <allandpn@hotmail.com>
2025-12-16 16:03:41 -0300

    s13

---
+++ src/main/java/s13/entities/Shape.java
@@ -0,0 +1,10 @@
+package s13.entities;
+
+
+import s13.enums.Color;
+
+public abstract class Shape {
+    private Color color;
+
+    public abstract double area();
+}
---
+++ src/main/java/s13/enums/Color.java
@@ -0,0 +1,7 @@
+package s13.enums;
+
+public enum Color {
+    BLACK,
+    BLUE,
+    RED,
+}
